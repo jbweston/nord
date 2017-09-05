26b9c104b636f21afbb2a02cac696bd89b1791df
Joseph Weston <joseph.weston08@gmail.com>
2017-09-05 13:26:06 +0200

    add common files

---
+++ README.md
@@ -0,0 +1,14 @@
+Nord: NordVPN client
+====================
+
+**This package is a work in progress -- it is not in a usable state***
+
+## Overview
+Nord is a client for interacting with the [NordVPN](https://nordvpn.com)
+service. At its core is a high-level Python API that supports operations such
+as connecting to the server in a given country that is most likely to give you
+the best performance. Nord also contains components that expose this API as a
+command line tool, and as a web service and a frontend.
+
+## Prerequesites
+Python 3.6 and all of the packages in `requirements.txt`.
---
+++ setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2017 Joseph Weston
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from setuptools import setup
+
+with open('requirements.txt') as r:
+    requirements = [l.strip() for l in r.readlines()]
+
+setup(
+    name='nord',
+    version='0.1',
+    py_modules=['nord'],
+    install_requires=requirements,
+)
