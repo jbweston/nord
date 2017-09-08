52b6e571cfdd3cbf54a0c59ec7cb551d8493b7f5
Joseph Weston <joseph.weston08@gmail.com>
2017-09-08 16:43:45 +0200

    move nord API into a Client class
    This stops aiohttp complaining, because we are using an
    aiohttp.ClientSession instead of just making ad-hoc requests.
    
    Closes #16

--- nord/api.py
+++ nord/api.py
@@ -14,31 +14,22 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-"""Interface to the NordVPN web API."""
+"""Interface to the NordVPN web API.
+
+This module contains a single class, `Client`, which encapsulates
+all the methods provided by NordVPN.
+"""
 
 import aiohttp
 
+from ._version import get_versions
 from ._utils import async_lru_cache
 
 
-HOST = 'api.nordvpn.com'
 PORTS = dict(tcp=443, udp=1194)
-HEADERS = {
-    'User-Agent': 'NordVPN Client/0.0.1',
-}
 
 
 # Low-level utilities
-
-def _url(endpoint):
-    return f'https://{HOST}/{endpoint}'
-
-
-async def _get(endpoint):
-    resp = await aiohttp.request('GET', _url(endpoint), headers=HEADERS)
-    resp.raise_for_status()
-    return resp
-
 
 def normalized_hostname(hostname):
     """Return the fully qualified domain name of a NordVPN host."""
@@ -59,90 +50,122 @@ def _openvpn_compatible(host):
     return features['openvpn_udp'] and features['openvpn_tcp']
 
 
-# NordVPN API
+class Client:
+    """Interface to the NordVPN web API.
 
-@async_lru_cache()
-async def host_config(host, protocol='tcp'):
-    """Return the OpenVPN config file contents for a NordVPN host.
-
-    Parameters
-    ----------
-    host : str
-        This hostname may be provided either with or without
-        the trailing '.nordvpn.com'.
-    protocol: str, 'tcp' or 'udp'
-    """
-    host = normalized_hostname(host)
-    resp = await _get(f'files/download/{_config_filename(host, protocol)}')
-    return await resp.text()
-
-
-async def host_load(host=None):
-    """Return the load on a NordVPN host.
+    Instances of this class can be used as async context managers
+    to auto-close the session with the Nord API on exit.
 
     Parameters
     ----------
-    host : str, optional
-        This hostname may be provided either with or without the trailing
-        '.nordvpn.com'. If not provided, get the load on all NordVPN hosts.
-
-    Returns
-    -------
-    load : int or (dict: str → int)
-        If 'host' was provided, returns the load on the host as
-        a percentage, otherwise returns a map from hostname
-        to percentage load.
+    api_url : str, default: 'https://api.nordvpn.com'
     """
-    if host:
+    def __init__(self, api_url='http://api.nordvpn.com/'):
+        self.api_url = api_url
+        client_version = get_versions()['version'].split('+')[0]
+        self.headers = {
+            'User-Agent': f"nord/{client_version}"
+        }
+        self._session = aiohttp.ClientSession(raise_for_status=True)
+
+    def close(self):
+        """Close the underlying aiohttp.ClientSession."""
+        self._session.close()
+
+    async def __aenter__(self):
+        return self
+
+    async def __aexit__(self, exc_typ, exc, traceback):
+        self.close()
+
+    async def _get(self, endpoint):
+        return self._session.get(''.join(self.api_url, endpoint),
+                                 headers=self.headers)
+
+    # API methods
+
+    @async_lru_cache()
+    async def host_config(self, host, protocol='tcp'):
+        """Return the OpenVPN config file contents for a NordVPN host.
+
+        Parameters
+        ----------
+        host : str
+            This hostname may be provided either with or without
+            the trailing '.nordvpn.com'.
+        protocol: str, 'tcp' or 'udp'
+        """
         host = normalized_hostname(host)
-    endpoint = f'server/stats/{host}' if host else 'server/stats'
-    resp = await _get(endpoint)
-    resp = await resp.json()
-    if host:
-        if len(resp) != 1:
-            # Nord API returns load on all hosts if 'host' does not exist.
-            raise KeyError(f'{host} does not exist')
-        return resp['percent']
-    else:
-        return {host: load['percent'] for host, load in resp.items()}
+        resp = await self._get(f'files/download/{_config_filename(host, protocol)}')
+        return await resp.text()
 
 
-async def current_ip():
-    """Return our current public IP address, as detected by NordVPN."""
-    resp = await _get('user/address')
-    return await resp.text()
+    async def host_load(self, host=None):
+        """Return the load on a NordVPN host.
+
+        Parameters
+        ----------
+        host : str, optional
+            This hostname may be provided either with or without the trailing
+            '.nordvpn.com'. If not provided, get the load on all NordVPN hosts.
+
+        Returns
+        -------
+        load : int or (dict: str → int)
+            If 'host' was provided, returns the load on the host as
+            a percentage, otherwise returns a map from hostname
+            to percentage load.
+        """
+        if host:
+            host = normalized_hostname(host)
+        endpoint = f'server/stats/{host}' if host else 'server/stats'
+        resp = await self._get(endpoint)
+        resp = await resp.json()
+        if host:
+            if len(resp) != 1:
+                # Nord API returns load on all hosts if 'host' does not exist.
+                raise KeyError(f'{host} does not exist')
+            return resp['percent']
+        else:
+            return {host: load['percent'] for host, load in resp.items()}
 
 
-@async_lru_cache()
-async def host_info():
-    """Return detailed information about all hosts.
-
-    Returns
-    -------
-    host_info : (dict: str → dict)
-        A map from hostnames to host info dictionaries.
-    """
-    resp = await _get('server')
-    info = await resp.json()
-    return {h['domain']: h for h in info}
+    async def current_ip(self):
+        """Return our current public IP address, as detected by NordVPN."""
+        resp = await self._get('user/address')
+        return await resp.text()
 
 
-@async_lru_cache()
-async def dns_servers():
-    """Return a list of ip addresses of NordVPN DNS servers."""
-    resp = await _get('dns/smart')
-    return await resp.json()
+    @async_lru_cache()
+    async def host_info(self):
+        """Return detailed information about all hosts.
+
+        Returns
+        -------
+        host_info : (dict: str → dict)
+            A map from hostnames to host info dictionaries.
+        """
+        resp = await self._get('server')
+        info = await resp.json()
+        return {h['domain']: h for h in info}
 
 
-async def valid_credentials(username, password):
-    """Return True if NordVPN accepts the username/password combination.
+    @async_lru_cache()
+    async def dns_servers(self):
+        """Return a list of ip addresses of NordVPN DNS servers."""
+        resp = await self._get('dns/smart')
+        return await resp.json()
 
-    Sometimes connecting to the VPN server gives an authentication error even
-    if the correct credentials are given. This function is useful to first verify
-    credentials so as to avoid unecessary reconnection attempts.
 
-    Parameters
-    ----------
-    username, password : str
-    """
-    return True
+    async def valid_credentials(self, username, password):
+        """Return True if NordVPN accepts the username/password combination.
+
+        Sometimes connecting to the VPN server gives an authentication error even
+        if the correct credentials are given. This function is useful to first verify
+        credentials so as to avoid unecessary reconnection attempts.
+
+        Parameters
+        ----------
+        username, password : str
+        """
+        return True
